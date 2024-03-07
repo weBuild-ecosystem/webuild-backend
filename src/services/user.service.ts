6e7dae40ade8a08365fcb370bbe263e92f5e140b
NickGroenl <bartocanitrot1@hotmail.com>
2024-03-06 21:38:38 -0300

    Create User endpoint

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -28,3 +28,25 @@ export function createUser(req: Request | any, res: Response) {
     return res.status(505).json({ message: "Invalid body or error" });
   }
 }
+
+export function cresateUser(req: Request | any, res: Response) {
+  try {  
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const body = req?.body;
+    const addingUser = new dealerSchema({
+      ...body
+    });
+    addingUser.markModified("users");
+    addingUser.save()
+    if (addingUser) {
+      return res
+        .status(202)
+        .json({ message: "User registered", user: addingUser});
+    } else return res.status(204).json({ message: "User not registered" });
+  } catch (errors) {
+    return res.status(505).json({ message: "Invalid body or error" });
+  }
+}
